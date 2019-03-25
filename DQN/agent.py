8786f2786b82be791d30c9b8132d47df11809724
wang yongjie <yongjie.wang@ntu.edu.sg>
2019-03-25 19:39:21 +0800

    agent.py

---
+++ DQN/agent.py
@@ -0,0 +1,79 @@
+import gym
+from collections import deque
+from util import *
+import random
+from net import *
+
+class Agent(object):
+
+    def __init__(self, env_name, gamma, memory_size, 
+            exploration_rate, exploration_min, exploration_decay,
+            learning_rate, action_space, state_space, batch_size):
+
+        self.gamma = gamma
+        self.memory_size = memory_size
+        self.exploration_rate = exploration_rate
+        self.exploration_min = exploration_min
+        self.exploration_decay = exploration_decay
+        self.batch_size = batch_size
+
+        self.QNet = DQN(learning_rate, action_space, state_space)
+        self.memory = deque(maxlen = memory_size)
+
+        self.env = gym.make(env_name)
+
+    def act(self, state):
+        if np.random.rand() < self.exploration_rate:
+            return random.randrange(self.action_space)
+        q_values = self.QNet.forward(state)
+        return np.argmax(q_values[0])
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def experience_replay(self):
+        if len(self.memory) < self.batch_size:
+            return
+
+        batch = random.sample(self.memory, self.batch_size)
+        for state, action, reward, state_next, terminal in batch:
+            q_update = reward
+            if not terminal:
+                q_update = (reward + self.gamma * np.max(self.QNet.forward(state_next)[0]))
+            q_values = self.QNet.forward(state)
+            q_values[0][action] = q_update
+            self.QNet.backward(state, q_values)
+
+        self.exploration_rate *= self.experience_decay
+        self.exploration_rate = max(self.exploration_rate, self.exploration_min)
+
+
+    def run(__init__):
+        episode = 0
+        while True:
+            episode += 1
+            real_state = self.env.reset()
+            state = preprocoss(real_state)
+            state = state[np.newaxis, :]
+            step = 0
+            if (episode + 1) % 50 == 0:
+                render = True
+            while True:
+                if render:
+                    self.env.render()
+                step += 1
+                action = self.act(state)
+                state_next, reward, terminal, info = self.env.step(action)
+                reward = reward if not terminal else -reward
+                state_next = preprocoss(state_next)
+                state_next = state_next[np.newaxis, :]
+                state = state_next
+
+                if terminal:
+                    print("Run: " + str(run) + ", exploration: " + str(self.exploration_rate) + ", score" + str(step))
+                    break
+                self.experience_replay()
+
+
+if __name__ == "__main__":
+
