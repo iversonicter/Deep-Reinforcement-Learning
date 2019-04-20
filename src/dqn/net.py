f77525da465cbfdae6e997190943cf54cf73dae3
yongjie wang <yongjie.wang@ntu.edu.sg>
2019-04-20 14:21:52 +0700

    net.py

--- src/dqn/net.py
+++ src/dqn/net.py
@@ -14,14 +14,17 @@ class Net(nn.Module):
         self.state_space = state_space
         self.action_space = action_space
 
-        self.fc1 = nn.Linear(self.state_space, 10) # 10 hidden units
-        self.fc2 = nn.Linear(10, self.action_space)
+        self.fc1 = nn.Linear(self.state_space, 24) # 10 hidden units
+        self.fc2 = nn.Linear(24, 24) # 10 hidden units
+        self.fc3 = nn.Linear(24, self.action_space)
         
 
     def forward(self, x):
         fc1 = self.fc1(x)
         relu1 = F.relu(fc1)
-        output = self.fc2(fc1)
+        fc2 = self.fc2(relu1)
+        relu2 = F.relu(fc2)
+        output = self.fc3(relu2)
 
         return output
 
