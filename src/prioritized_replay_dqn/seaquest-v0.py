4616cafefedb754aed3cfc9d2aa84e970df20ac2
yongjie wang <yongjie.wang@ntu.edu.sg>
2019-04-25 14:52:37 +0700

    seaquest-v0.py

--- src/prioritized_replay_dqn/seaquest-v0.py
+++ src/prioritized_replay_dqn/seaquest-v0.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
             memory_size = 100000, replace_iter = 20,
             exploration_rate = 1, exploration_min = 0.01,
             exploration_decay = 0.999, lr = 0.0002, batch_size = 20,
-            is_render = False)
+            is_render = True)
     seaquest.run()
     seaquest.plot()
     seaquest.close()
