52336c655da3bc9abc50e7a7b506331d6aee68fa
yongjie wang <yongjie.wang@ntu.edu.sg>
2019-04-25 17:39:15 +0700

    seaquest-v0.py

--- src/prioritized_replay_dqn/seaquest-v0.py
+++ src/prioritized_replay_dqn/seaquest-v0.py
@@ -3,9 +3,9 @@ from agent import Prioritized_Double_DQN
 if __name__ == "__main__":
 
     seaquest = Prioritized_Double_DQN("Seaquest-v0", gamma = 0.95,
-            memory_size = 100000, replace_iter = 20,
-            exploration_rate = 1, exploration_min = 0.01,
-            exploration_decay = 0.999, lr = 0.0002, batch_size = 20,
+            memory_size = 100000, replace_iter = 200,
+            exploration_rate = 1, exploration_min = 0.1,
+            exploration_max = 1, lr = 0.0002, batch_size = 20,
             is_render = True)
     seaquest.run()
     seaquest.plot()
