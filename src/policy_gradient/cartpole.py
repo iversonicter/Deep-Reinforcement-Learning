d5924730e0f024e37fac17b8ef3f73bebde53d1d
yongjie wang <yongjie.wang@ntu.edu.sg>
2019-04-27 10:39:48 +0700

    cartpole.py

---
+++ src/policy_gradient/cartpole.py
@@ -0,0 +1,31 @@
+# Author: Wang Yongji
+# Email : yongjie.wang@ntu.edu.sg
+# Description: cartpole 
+
+import gym
+from policy_gradient import Agent
+from net import Net
+
+env = gym.make('CartPole-v0')
+env.seed(1)
+is_render = False
+
+eval_net = Net(env.observation_space.shape, env.action_space.n)
+agent = Agent(eval_net, lr = 0.02, reward_decay = 0.99)
+
+for i in range(300):
+    observation = env.reset()
+    total_reward = 0
+
+    while True:
+        action = agent.choose_action(observation)
+        observation_, reward, done, info = env.step(action)
+        agent.store_transition(observation, action, reward)
+        total_reward += reward
+        observation = observation_
+        if done:
+            agent.learn()
+            print("episode: ", i, "\t total reward: ", total_reward)
+            break
+
+
